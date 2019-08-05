aa17b5ec6c17b9aa74bfdf9203ad9764f162e6b1
Jiahong Wang <wangjiahong666@gmail.com>
2019-08-06 00:30:23 +0300

    Overwrite old ids, instead of append
    Otherwise old videos will be downloaded.

--- fetch_html_ranking_html.sh
+++ fetch_html_ranking_html.sh
@@ -1,4 +1,4 @@
 curl --request GET \
   --url https://www.bilibili.com/ranking/all/0/0/30 \
   --header 'cache-control: no-cache' \
-  --header 'postman-token: 50a95bd2-245b-384b-b6b0-68000878c071' >> result.txt
+  --header 'postman-token: 50a95bd2-245b-384b-b6b0-68000878c071' > result.txt
