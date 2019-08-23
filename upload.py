e867e232642d1b33bc54f10da68c8609d308c94b
Jiahong Wang <wangjiahong666@gmail.com>
2019-08-23 22:13:04 +0300

    save before debug

--- upload.py
+++ upload.py
@@ -2,39 +2,38 @@
 
 import re
 import os
+import datetime
 
 
 path = './'
-
 files = []
+
 # r=root, d=directories, f = files
 for r, d, f in os.walk(path):
     for file in f:
         if '.mp4' in file:
             files.append(os.path.join(r, file))
 
-uploaded_videos = open("uploaded_videos.txt","a")
+uploaded_videos = open("logs_uploaded_videos.txt","a")
 
 
-for f in files[5:7]:
-    #print(f)
-    #print(type(f))
-
+for f in files[11:12]:
     title = f.replace('./downloads/', '').replace('.mp4', '')
-    #print title
-
-
     tags = "B站, Bilibili"
-    #print("Shepherd {} is years old.".format(tags))
-
     upload_script = 'youtube-upload --title="#B站 #Bilibili {}" --tags="{}" --description="{} #B站 #Bilibili" {}'.format(title, tags, title, f)
     print(upload_script)
     print
     print
     print("{}.mp4".format(title))
-    #os.system("'{}.mp4' >> uploaded_videos.txt".format(title))
-
     os.system(upload_script)
 
-    uploaded_videos.write(title + "\n")
+    # Save the title of uploaded videos to logs files
+    datetime_now = str(datetime.datetime.now())
+    uploaded_videos.write('{}, {}'.format(datetime_now, title)
+    # Remove the uploaded video from local file
+    # print('rm ./downloads/{}'.format(title))
+    print(123)
+    print
+    print('removed uploaded video: {}'.format(title))
+
 uploaded_videos.close()
