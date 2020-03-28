9be072c7a17eff0f193558068714001e051cf1c0
Jiahong Wang <wangjiahong666@gmail.com>
2020-03-28 14:13:39 +0200

    Update download_videos.py
    Get videoId and titles;
    skip download if video exists;
    print more info

--- download_videos.py
+++ download_videos.py
@@ -1,13 +1,54 @@
 import re
 import os
 
+
+# f7 is the function remove the duplicates of list, but perserve the order of items
+def f7(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
+
+
+# Get video id
 with open('result.txt', 'r') as file:
     data = file.read().replace('\n', '')
+    print(data)
+ids = re.findall('(?:video/)[0-9a-zA-Z]+', data)
+print(ids)
+ids = f7(ids)
 
-ids = re.findall('(?:av)[0-9]+', data)
-print (ids)
-for i in range(len(ids)):
+# Get titles and write to file
+titles = re.findall('"title">(.*?)</', data)
+titles = f7(titles)
+print (titles)
+with open('Video_titles.txt', 'w') as f:
+    for item in titles:
+        f.write("%s\n" % item)
+
+
+# Download videos
+for i in range(len(ids)): #
+	print ("{} th vodeo, processing and downloading...".format(i))
 	print ("*" * 30)
-	url = "https://www.bilibili.com/video/" + ids[i]
-	shell_script =  "you-get '"+url+"' -o downloads"
-	os.system(shell_script)
+	url = "https://www.bilibili.com/" + ids[i]
+	all_downloaded_files_txt = '-'.join(os.listdir('./downloads'))
+
+	print ("   " + url)
+	try:
+		print ("   " + titles[i])
+	except: 
+		pass
+
+	if titles[i] not in all_downloaded_files_txt:
+		shell_script =  "you-get '"+ url +"' -o downloads"
+		os.system(shell_script)
+		print ("downloaded!")
+	else:
+		pass
+	print ("\n\n")
+
+print ("we get {} videos and {} titles.".format(len(ids), len(titles)))
+print ("Cheers, Completed !!!!!")
+
+
+
