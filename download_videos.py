4cf371475bddbc35582d42a9517e1187e8ee98d9
Jiahong Wang <wangjiahong666@gmail.com>
2020-03-28 12:51:27 +0200

    Fix python 3 print ()

--- download_videos.py
+++ download_videos.py
@@ -5,9 +5,9 @@ with open('result.txt', 'r') as file:
     data = file.read().replace('\n', '')
 
 ids = re.findall('(?:av)[0-9]+', data)
-print ids
+print (ids)
 for i in range(len(ids)):
-	print "*" * 30
+	print ("*" * 30)
 	url = "https://www.bilibili.com/video/" + ids[i]
 	shell_script =  "you-get '"+url+"' -o downloads"
 	os.system(shell_script)
